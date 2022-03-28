6bffb347eccd7c54b539981c13fe6d7aa3829809
Nuogz <zheung@vip.qq.com>
2022-03-28 23:16:39 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,71 @@
+import Wock from './lib/Wock.js';
+
+
+class StaffWock {
+	wock;
+
+	app;
+	token;
+
+	interval;
+	staff;
+	timer;
+
+
+	constructor(target, app, token, interval, staff) {
+		if(!~~interval || ~~interval < 1000) { throw Error('间隔无效或小于一秒'); }
+
+
+		this.app = app;
+		this.token = token;
+
+		this.interval = interval;
+		this.staff = staff;
+
+
+		const wock = this.wock = new Wock(new URL('wock', `http://${target.host}:${target.port}`).toString().replace(/^http/, 'ws'));
+
+
+		wock.add('start', () => {
+			clearInterval(this.timer);
+
+			this.timer = setInterval(this.runner.bind(this), this.interval);
+
+			this.runner();
+		});
+
+		wock.add('stop', () => clearInterval(this.timer));
+
+		wock.add('setToken', token => {
+			this.token = token;
+
+			this.auth();
+		});
+
+
+		wock.reopen = this.auth.bind(this);
+		wock.at('open', this.auth.bind(this), true);
+	}
+
+	auth() {
+		if(this.token) {
+			this.wock.cast('hey/auth-staff', this.app, this.token);
+		}
+	}
+
+	runner() {
+		const push = this.staff(this);
+
+		if(push) {
+			this.hey(push);
+		}
+	}
+
+	hey(push) { return this.wock.cast('hey/push', push); }
+
+	add(...params) { return this.wock.add(...params); }
+	open(reason) { return this.wock.open(reason); }
+}
+
+
+export default StaffWock;
